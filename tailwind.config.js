2f8da5ef8efe96f349f122ad97c9956b6244f0cb
Phil J Gray <phl.j.gray@gmail.com>
2023-05-27 17:42:49 -0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  //paths to all of your template files
+  content: ["./app/**/*.{js,ts,jsx,tsx}",],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
